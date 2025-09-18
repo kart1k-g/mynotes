41215ed5960591f5cf4c659950e48c7efe672ddf
Kartik <kart1kg.create@gmail.com>
2025-09-18 19:09:28 +0530

    add email verification for the current user

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/Views/login_view.dart';
+import 'package:mynotes/Views/register_view.dart';
 import 'package:mynotes/firebase_options.dart';
 void main() {
   // runApp(const MyApp());
@@ -10,7 +12,9 @@ void main() {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage(),
+      // home: const RegisterView(),
+      home: const LoginView(),
+      // home: const HomePage(),
     ));
 }
 
@@ -38,10 +42,12 @@ class _HomePageState extends State<HomePage> {
           switch(snapshot.connectionState){
             case ConnectionState.done:
               final user=FirebaseAuth.instance.currentUser;
+              print(user);
               if(user?.emailVerified??false){
                 print("Email Verified");
               }else{
                 print("Verify your email first");
+                return const VerifyEmailView();
               }
               return const Text("Done");
             default:
@@ -54,4 +60,33 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Text("Verify your email first"),
+      Text(FirebaseAuth.instance.currentUser+""),
+      TextButton(
+        onPressed: ()async{
+            final user=FirebaseAuth.instance.currentUser;
+            await user?.sendEmailVerification();
+        },
+        child: const Text("Send email verification")
+      )
+    ],);
+  }
+}
+
+extension on User? {
+  String operator +(String other) {
+    return (this?.email ?? 'No Email') + other;
+  }
+}
 
