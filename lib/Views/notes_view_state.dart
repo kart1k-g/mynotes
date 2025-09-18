8224b0234562b024e183b374de7d4c2b11f5e12b
Kartik <kart1kg.create@gmail.com>
2025-09-18 22:23:31 +0530

    add notes view (main ui page) with logout option in it

---
+++ lib/Views/notes_view_state.dart
@@ -0,0 +1,75 @@
+
+import 'dart:developer' as devtools show log;
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+enum MenuAction{logout}
+
+class NotesViewState extends StatefulWidget {
+  const NotesViewState({super.key});
+
+  @override
+  State<NotesViewState> createState() => _NotesViewStateState();
+}
+
+class _NotesViewStateState extends State<NotesViewState> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main UI"), 
+          backgroundColor: Colors.deepPurple,
+          actions: [
+            PopupMenuButton<MenuAction>(
+              onSelected: (value)async{
+                // print(value);
+                // devtools.log(value.toString() );
+                switch(value){
+                  case MenuAction.logout:
+                    final shouldLogOut=await showLogOutDailog(context);
+                    devtools.log(shouldLogOut.toString());
+                    if(shouldLogOut){
+                      FirebaseAuth.instance.signOut();
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        "/login/",
+                        (_)=> false,
+                      );
+                    }
+                }
+              },
+              itemBuilder: (context){
+                return const [
+                  PopupMenuItem<MenuAction>(
+                    value: MenuAction.logout,
+                    child: Text("Logout"),
+                  ),
+                ];
+              })
+          ],
+      ),
+      body: const Text("UI"),
+  
+    );
+  }
+}
+
+Future<bool> showLogOutDailog(BuildContext context){
+  return showDialog(
+    context: context,
+    builder: (context){
+      return AlertDialog(
+        title: const Text("Log Out"),
+        content: const Text("Are you sure you want to log out?"),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.of(context).pop(false);
+          }, child: const Text("Cancel")),
+          TextButton(onPressed: (){
+            Navigator.of(context).pop(true);
+          }, child: const Text("Log Out")),
+        ],
+      );
+    }).then((value) => value??false);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/Views/login_view.dart';
+import 'package:mynotes/Views/notes_view_state.dart';
 import 'package:mynotes/Views/register_view.dart';
 import 'package:mynotes/Views/verify_email_view.dart';
 import 'package:mynotes/firebase_options.dart';
+import 'dart:developer' as devtools show log;
 void main() {
   // runApp(const MyApp());
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,35 +36,32 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Home"), backgroundColor: Colors.deepPurple,),
-      body: FutureBuilder(
-          future: Firebase.initializeApp(
-              options: DefaultFirebaseOptions.currentPlatform,
-          ),
-      
-          builder: (context, snapshot) {
-            switch(snapshot.connectionState){
-              case ConnectionState.done:
-                final user=FirebaseAuth.instance.currentUser;
-                // print(user);
-                if(user!=null){
-                  if(user.emailVerified){
-                    print("Email Verified");
-                  }else{
-                    return const VerifyEmailView();
-                  }
-                }else{
-                  return const LoginView();
-                }
-                return const Text("done");
-              default:
-                // return const Text("Loading...");
-                return CircularProgressIndicator();
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+      ),
+  
+      builder: (context, snapshot) {
+        switch(snapshot.connectionState){
+          case ConnectionState.done:
+            final user=FirebaseAuth.instance.currentUser;
+            // print(user);
+            if(user!=null){
+              if(user.emailVerified){
+                print("Email Verified");
+                return NotesViewState();
+              }else{
+                return const VerifyEmailView();
+              }
+            }else{
+              return const LoginView();
             }
-          },
+          default:
+            // return const Text("Loading...");
+            return CircularProgressIndicator();
+        }
+      },
           
-        ),
     );
   }
 }
